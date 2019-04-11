607cc9c487e1016bf4565591f58870c2c8b7b888
Daniel Piña <32937837+DanpinaUCM@users.noreply.github.com>
2019-04-11 13:04:14 +0200

    Update startondb.sql
    Faltaba linea de añadir base de datos si esta no existe (para el importe a xampp)

--- mysql/startondb.sql
+++ mysql/startondb.sql
@@ -21,7 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `startondb`
 --
-
+CREATE DATABASE IF NOT EXISTS `startondb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `startondb`;
 -- --------------------------------------------------------
 
 --
