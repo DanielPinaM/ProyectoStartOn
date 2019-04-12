0c8f16c996a08f11c0c0a217db8733a597a065ff
Daniel Piña <32937837+DanpinaUCM@users.noreply.github.com>
2019-04-12 15:04:50 +0200

    conocenos.php creado
    Sin css, solo html (basado en entrenga 1)

---
+++ Vista/conocenos.php
@@ -0,0 +1,102 @@
+<?php require_once ("../includes/config.php"); ?>
+<!DOCTYPE html>
+<html>
+  <link rel="stylesheet" type="text/css" href="css/common.css">
+  <meta charset="utf-8">
+
+  <head>
+  	<title>Start On</title>
+  </head>
+
+  <body>
+  	<div class="container">
+  		<?php require("common/header.php")?>
+
+  		<div class="row"> <!--Row titulo-->
+  			<div class="titulo"> <!--Div Titulo-->
+  				Conócenos
+  			</div>
+  		</div>
+
+  		<div class="row"> <!--/Row body-->
+        <div> <!--Div Parrafo-->
+  				<p>Esta página ha sido realizada como parte de un proyecto universitario.</p>
+          <p>Universidad Complutense de Madrid.</p>
+  			</div>
+      </div>
+
+      <div class="row"> <!--/Row body-->
+        <div> <!--Div Parrafo-->
+  				<p>Nuestro proyecto se trata de una web para poner en contacto a personas interesadas en el mundo del emprendimiento que estén el la etapa laboral de su vida con startups en cualquier fase que quieran buscar a nuevos integrantes en sus equipos.</p>
+  			</div>
+
+        <div class="row"> <!--Row titulo-->
+    			<div class="titulo"> <!--Div Titulo-->
+    				Nuestro equipo
+    			</div>
+    		</div>
+
+
+      <div class="bio_miembro">
+        <h2>Pablo Aguilera Heredero</h2>
+	   			<h3><i>pablagui@ucm.es</i></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum sapien eu sem cursus, vitae euismod ex vulputate. Nunc pellentesque, ex nec egestas eleifend, purus dui volutpat enim, non elementum nisl massa viverra turpis. Pellentesque bibendum justo at tristique accumsan. Nunc rhoncus quam id enim.</p>
+			</div>
+   		</div>
+		<div class="row small_white_space"></div>
+		<div class="row content"  id="pers2">
+			<div class="bio_miembro">
+				<h2>Pablo Fernández Jara</h2>
+	   			<h3><i>pablof10@ucm.es</i></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum sapien eu sem cursus, vitae euismod ex vulputate. Nunc pellentesque, ex nec egestas eleifend, purus dui volutpat enim, non elementum nisl massa viverra turpis. Pellentesque bibendum justo at tristique accumsan. Nunc rhoncus quam id enim.</p>
+			</div>
+		</div>
+		<div class="row small_white_space"></div>
+		<div class="row content"  id="pers3">
+			<div class="bio_miembro">
+				<h2>Eric Heresi Medina</h2>
+	   			<h3><i>eheresi@ucm.es</i></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce congue nisl imperdiet, ullamcorper urna nec, vestibulum lacus. Etiam nulla lorem, viverra at sem vel, condimentum consectetur nibh. Vestibulum egestas, sem sit amet suscipit vestibulum, sapien ex aliquet enim, ac placerat.</p>
+			</div>
+		</div>
+		<div class="row small_white_space"></div>
+		<div class="row content"  id="pers4">
+			<div class="bio_miembro">
+				<h2>Jennifer Marmolejos Urbaz</h2>
+	   			<h3><i>jenmarmo@ucm.es</i></h3>
+				<p>Alumna de Ingenieria Informática,me encanta programar por libre en lenguajes como Swift o Python, resolver problemas de algoritmia es mi pasión!! </p>
+			</div>
+		</div>
+		<div class="row small_white_space"></div>
+		<div class="row content"  id="pers5">
+			<div class="bio_miembro">
+				<h2>Cristian Molina Muñoz</h2>
+	   			<h3><i>crismo04@ucm.es</i></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum sapien eu sem cursus, vitae euismod ex vulputate. Nunc pellentesque, ex nec egestas eleifend, purus dui volutpat enim, non elementum nisl massa viverra turpis. Pellentesque bibendum justo at tristique accumsan. Nunc rhoncus quam id enim.</p>
+			</div>
+		</div>
+		<div class="row small_white_space"></div>
+		<div class="row content"  id="pers6">
+			<div class="bio_miembro">
+				<h2>Daniel Piña Miguelsanz</h2>
+	   			<h3><i>danpina@ucm.es</i></h3>
+				<p>Estusiasta del emprendimiento y estudiante de ingenieria informática. Trabajando duro y aprendiendo día a día para construir mi camino, centrándome en lo que de verdad me importa en el recorrido.</p>
+			</div>
+		</div>
+		<div class="row small_white_space"></div>
+		<div class="row content" id="pers7">
+			<div class="bio_miembro">
+				<h2>Antonio Rodríguez Hurtado</h2>
+	   			<h3><i>antrod03@ucm.es</i></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum sapien eu sem cursus, vitae euismod ex vulputate. Nunc pellentesque, ex nec egestas eleifend, purus dui volutpat enim, non elementum nisl massa viverra turpis. Pellentesque bibendum justo at tristique accumsan. Nunc rhoncus quam id enim.</p>
+			</div>
+		</div>
+		<div class="row white_space"></div>
+	</div>
+  		</div>
+
+  		<?php require("common/footer.php")?>
+
+  	</div>
+  </body>
+</html>
