e6072cb04b8f1ad622f722ff1c85f10acddd1add
crismom <47787192+crismom@users.noreply.github.com>
2019-04-12 17:03:34 +0200

    correccion header

--- Vista/common/header.php
+++ Vista/common/header.php
@@ -1,22 +1,34 @@
 <?php if(!isset($_SESSION['login'])) $_SESSION['login']=false ?>
-<div>
-	<ul class="nav_bar">
-		<li><a class="button" href="/ProyectoStartOn/index.php">Inicio</a>
-		<li><a class="button" href="/ProyectoStartOn/vista/listEmpresa.php">Startups</a>
-		<li><a class="button" href="/ProyectoStartOn/vista/listUser.php">Usuarios</a>
-		<li><a class="button" href="">Eventos</a>
-		<li><a class="button" href="/ProyectoStartOn/vista/conocenos.php">Conócenos</a>
-		<li><a class="button" href="">Ayuda</a>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/common.css">
+	<title>Start On</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<div id="cabecera">
+	<ul>
+		<li><img id='logo_cabecera' src="/ProyectoStartOn/img/Logo2.png">
+		<li><a href="/ProyectoStartOn/index.php">Inicio</a>
+		<li><a href="/ProyectoStartOn/vista/listEmpresa.php">Startups</a>
+		<li><a href="/ProyectoStartOn/vista/listUser.php">Usuarios</a>
+		<li><a href="">Eventos</a>
+		<li><a href="/ProyectoStartOn/vista/conocenos.php">Conócenos</a>
+		<li><a href="">Ayuda</a>
 		<?php
 			if(!$_SESSION['login'])
-				echo '<li style="float:right"><a id="inicio_sesion" class="button" href="login.php">Inicia sesión</a>';
+				echo '<li style="float:right"><a id="inicio_sesion" href="/ProyectoStartOn/vista/login.php">Inicia sesión</a>';
 			else{
-				echo '<li style="float:right"><a id="inicio_sesion" class="button" href="logout.php">Cerrar sesión</a>';
+				echo '<li style="float:right"><a id="inicio_sesion" href="/ProyectoStartOn/vista/logout.php">Cerrar sesión</a>';
 				if(isset($_SESSION['id_usuario']))
-					echo '<li style="float:right"><a id="inicio_sesion" class="button" href="perfUser.php">Perfil</a>';
+					echo '<li style="float:right"><a id="inicio_sesion" href="/ProyectoStartOn/vista/perfUser.php">Perfil</a>';
 				else
-					echo '<li style="float:right"><a id="inicio_sesion" class="button" href="perfEmp.php">Perfil</a>';
+					echo '<li style="float:right"><a id="inicio_sesion"  href="/ProyectoStartOn/vista/perfEmp.php">Perfil</a>';
 			}
 		?>
 	</ul>
-</div>
+	</div
+</body>
+</html>
