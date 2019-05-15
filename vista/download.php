ec0722317f3c871f30dfa391de7f535317994430
tonyrh38 <antrodrigh@gmail.com>
2019-05-15 15:01:59 +0200

    Mejora de la descarga de CV

--- vista/download.php
+++ vista/download.php
@@ -1,10 +1,20 @@
 <?php
+require_once ("../includes/config.php");
+require_once ("../logica/SA_Usuario.php");
+
 if(isset($_GET["id"])){
-	header("Content-disposition: attachment; filename=curriculum.pdf");
-	header("Content-type: application/pdf");
-	readfile("../pdf/curr".$_GET['id'].".pdf");
+	$SA = SA_Usuario::getInstance();
+	$usr = $SA->getElement($_GET["id"]);
+	if($usr != null && $usr->getCurriculum() != null){
+		header("Content-disposition: attachment; filename=curriculum.pdf");
+		header("Content-type: application/pdf");
+		readfile("../pdf/curr".$_GET['id'].".pdf");
+	}
+	else{
+		header("Location: perfUser.php?id=".$_GET["id"]);
+	}
 }
 else{
-	header("Location: index.php");
+	header("Location: perfUser.php?id=".$_GET["id"]);
 }
 ?>
